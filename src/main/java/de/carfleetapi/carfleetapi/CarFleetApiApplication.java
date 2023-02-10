7074f6e91f5b3dac38466e480960791ff1ac3ea6
Programm0r <filmproduction@t-online.de>
2023-02-10 18:26:35 +0100

    First Commit

---
+++ src/main/java/de/carfleetapi/carfleetapi/CarFleetApiApplication.java
@@ -0,0 +1,13 @@
+package de.carfleetapi.carfleetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFleetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarFleetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/carfleetapi/carfleetapi/CarFleetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.carfleetapi.carfleetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFleetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
